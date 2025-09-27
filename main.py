df3bf25ad1e92751d980a42eab274c6d98da02e8
EchoHeli <emiliano.dorantest@gmail.com>
2025-09-26 19:28:16 -0600

    .

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+import juegoppt
+import juegoahorcado
+import snake
+
+def menu():
+    print("""Elige que juego quieres jugar:
+1. Ahorcado
+2. Piedra Papel Tijera
+3. Snake
+4. Tic Tac Toe""")
+    
+def main():
+    menu()
+    opt = int(input(""))
+    if opt == 1:
+        opt2 = int(input("Caso de prueba? (y/n): "))
+        opt2 = opt2.lower()
+        while True:
+            if opt2 == "y":
+                juegoahorcado.pruebas()
+                break
+            elif opt2 == "n":
+                juegoahorcado.main()
+                break
+            else:
+                print("Opción incorrecta, intente de nuevo\n")
+    elif opt == 2:
+        juegoppt.main()
+    elif opt == 3:
+        snake.main()
+    
+    
 
