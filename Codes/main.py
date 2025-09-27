3c46a96abe319022df23fad8c03485605f67e99d
EchoHeli <emiliano.dorantest@gmail.com>
2025-09-27 05:45:02 +0000

    Update main.py

--- Codes/main.py
+++ Codes/main.py
@@ -13,24 +13,28 @@ def menu():
 def main():
     menu()
     opt = int(input(""))
-    if opt == 1:
-        opt2 = int(input("Caso de prueba? (y/n): "))
-        opt2 = opt2.lower()
-        while True:
-            if opt2 == "y":
-                juegoahorcado.pruebas()
-                break
-            elif opt2 == "n":
-                juegoahorcado.main()
-                break
-            else:
-                print("Opción incorrecta, intente de nuevo\n")
-    elif opt == 2:
-        juegoppt.main()
-    elif opt == 3:
-        snake.main()
-    elif opt == 4:
-        TictactoeGames.jugar()
+    while True:
+        if opt == 1:
+            opt2 = int(input("Caso de prueba? (y/n): "))
+            opt2 = opt2.lower()
+            while True:
+                if opt2 == "y":
+                    juegoahorcado.pruebas()
+                    break
+                elif opt2 == "n":
+                    juegoahorcado.main()
+                    break
+                else:
+                    print("Opción incorrecta, intente de nuevo\n")
+        elif opt == 2:
+            juegoppt.main()
+            break
+        elif opt == 3:
+            snake.main()
+            break
+        elif opt == 4:
+            TictactoeGames.jugar()
+            break
 
-main()   
-    
+main() 
+
