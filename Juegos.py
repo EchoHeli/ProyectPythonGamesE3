f786bc7059e033f1ac3f3d4bf982e7a13d18c82e
EchoHeli <emiliano.dorantest@gmail.com>
2025-10-10 16:08:49 -0600

    PPT

--- Juegos.py
+++ Juegos.py
@@ -1,6 +1,6 @@
-import Piedra_Papel_Tijera
-from Ahorcado import jugar_ahorcado
-from Snake import snake
+from Piedra_Papel_Tijera import juegoppt
+import Ahorcado
+import Snake
 import Tic_Tac_Toe
 
 def menu():
@@ -18,14 +18,42 @@ def main():
             opt2 = input("Caso de prueba? (y/n): ")
             opt2 = opt2.lower()
             while True:
-                if opt2 == "n":
+                if opt2 == "y":
+                    Ahorcado.pruebas()
+                    break
+                elif opt2 == "n":
                     Ahorcado.main()
                     break
                 else:
                     print("Opción incorrecta, intente de nuevo\n")
             break
         elif opt == 2:
-            Piedra_Papel_Tijera.mainppt()
+            juegoppt.menuppt()
+            opt = int(input(""))
+            while True:
+                if opt == 1:
+                        n = int(input("Ingrese cuantos juegos quiere jugar en la serie (Impar): "))
+                        if n % 2 != 0:
+                                juegoppt.ppt(n)
+                                break
+                        else: 
+                                print("El numero de juegos en la serie debe de ser impar, intente de nuevo\n")
+                elif opt == 2:
+                        n = int(input("Ingrese cuantos juegos quiere jugar en la serie (Impar): "))
+                        if n % 2 != 0:
+                                juegoppt.pptdificil(n)
+                                break
+                        else: 
+                                print("El numero de juegos en la serie debe de ser impar, intente de nuevo\n") 
+                elif opt == 3:
+                        n = int(input("Ingrese cuantos juegos quiere jugar en la serie (Impar): "))
+                        if n % 2 != 0:
+                                juegoppt.pptls(n)
+                                break
+                        else: 
+                                print("El numero de juegos en la serie debe de ser impar, intente de nuevo\n") 
+                else:
+                        print("Opción invalida, intente de nuevo")
             break
         elif opt == 3:
             snake.main()
