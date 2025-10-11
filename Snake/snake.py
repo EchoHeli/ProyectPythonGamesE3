6024cb5899e0c4434acfff30af93824c8a46ed44
keli-gif <kelizabeth.oyoqui@gmail.com>
2025-10-10 21:07:00 -0600

    Update snake.py

--- Snake/snake.py
+++ Snake/snake.py
@@ -1,3 +1,51 @@
+"""
+INICIO
+
+Muestra las instrucciones del juego
+
+Pedir al usuario que escoga la dificultad (Fácil, Media o Difícil)
+→ Según la dificultad, definir el tamaño del tablero
+
+Colocamos la serpiente en el centro del tablero
+Inicializamos la dirección en la derecha
+Inicializamos el puntaje
+Generar una manzana en una posición aleatoria en donde no este la serpiente
+
+Limpiar pantalla
+Mostrar el tablero con la serpiente, la manzana y los puntos
+
+MIENTRAS el juego no ! == game over:
+    Pedir al usuario una dirección y número de pasos (ej. w 3)
+
+    SI la entrada no es válida:
+        Mostrar mensaje de error
+
+    Convertir la dirección y pasos a variables
+
+    REPETIR hasta game over:
+        Calcular la nueva posición de la cabeza de la serpiente
+         Insertar la nueva cabeza al inicio de la lista de la serpiente
+
+        SI la nueva cabeza está en la misma posición que la manzana:
+            Aumentar puntos en 1
+            Mostrar mensaje de que comió una manzana
+            Generar una nueva manzana en una posición libre
+        SINO:
+            Eliminar la última parte del cuerpo de la serpiente (no crece)
+
+        Limpiar pantalla
+        Mostrar el tablero actualizado con la serpiente, la manzana y los puntos obtenidos
+
+        SI la nueva posición está fuera del tablero o choca con el cuerpo:
+            Mostrar mensaje de choque
+            Mostrar puntaje final
+            Terminar el juego
+
+
+FIN
+Slida:
+Mensaje de game over y puntaje final
+"""
 import random
 import time
 import os
